176270416e1daea071ba651b9b28bc42e2454652
jeevaninallu <nallujeevanireddy@gmail.com>
2025-11-26 02:19:52 +0530

    Update load_dataset.py

--- ingestion/load_dataset.py
+++ ingestion/load_dataset.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import os
 
+RAW_DIR = "../dataset/raw"
+PROCESSED_DIR = "../dataset/processed"
+
+filename = "my_sensor_data.xlsx"
+file_path = os.path.join(RAW_DIR, filename)
+
+# Read Excel
+df = pd.read_excel(file_path)
+
+# Quick check
+print(df.head())
+print(df.info())
